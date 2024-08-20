b2b1f220bfe03a5e2012a19a72f45591ada6b3ae
agsjorda <agsjorda@gmail.com>
2024-08-20 16:54:58 +0800

    initial setup and folder structure

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// https://docs.expo.dev/guides/using-eslint/
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const home = () => {
+	return (
+		<SafeAreaView className="flex-1 justify-center items-center">
+			<Text>Home screen.</Text>
+		</SafeAreaView>
+	);
+};
+
+export default home;
