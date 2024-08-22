b4737cd499dce027cf4e27e15fbdad96d8c5ebef
agsjorda <agsjorda@gmail.com>
2024-08-23 04:37:42 +0800

    welcome screens

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,11 +4,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
 import { onboarding } from "@/constants";
-
+import CustomButton from "@/components/CustomButton";
 
 const Onboarding = () => {
 	const swiperRef = useRef<Swiper>(null);
 	const [activeIndex, setActiveIndex] = useState(0);
+	const isLastSlide = activeIndex === onboarding.length - 1;
 
 	return (
 		<SafeAreaView className="flex h-full items-center justify-between bg-white">
@@ -33,11 +34,29 @@ const Onboarding = () => {
 			>
 				{onboarding.map((item) => (
 					<View key={item.id} className="flex items-center justify-center p-5">
-						<Image source={item.image} className="w-full h-[300px]" resizeMode="contain"/>
-						<Text>{item.title}</Text>
+						<Image
+							source={item.image}
+							className="w-full h-[300px]"
+							resizeMode="contain"
+						/>
+						<View className="flex flex-grow items-center justify-center w-full mt-10">
+							<Text className="text-black text-3xl font-bold mx-10 text-center">
+								{item.title}
+							</Text>
+						</View>
+						<Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+							{item.description}
+						</Text>
 					</View>
 				))}
 			</Swiper>
+			<CustomButton
+				title={isLastSlide ? "Get Started" : "Next"}
+				onPress={() => {
+					isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current?.scrollBy(1);
+				}}
+				className="w-11/12 mt-10 "
+			/>
 		</SafeAreaView>
 	);
 };
