63cce3d9f492705e29c4e02e12bbfc00ca88556c
agsjorda <agsjorda@gmail.com>
2024-08-23 03:15:30 +0800

    added: -images -types -constants -basic onboarding screens

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,13 +1,45 @@
-import { Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import React, { useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { onboarding } from "@/constants";
+
 
 const Onboarding = () => {
-  return (
-    <SafeAreaView>
-      <Text>Onboarding</Text>
-    </SafeAreaView>
-  )
-}
+	const swiperRef = useRef<Swiper>(null);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-export default Onboarding
+	return (
+		<SafeAreaView className="flex h-full items-center justify-between bg-white">
+			<TouchableOpacity
+				onPress={() => {
+					router.replace("/(auth)/sign-up");
+				}}
+				className="w-full flex justify-end items-end p-5"
+			>
+				<Text className="text-black text-md font-JakartaBold">Skip</Text>
+			</TouchableOpacity>
+			<Swiper
+				ref={swiperRef}
+				loop={false}
+				dot={
+					<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+				}
+				activeDot={
+					<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+				}
+				onIndexChanged={(index) => setActiveIndex(index)}
+			>
+				{onboarding.map((item) => (
+					<View key={item.id} className="flex items-center justify-center p-5">
+						<Image source={item.image} className="w-full h-[300px]" resizeMode="contain"/>
+						<Text>{item.title}</Text>
+					</View>
+				))}
+			</Swiper>
+		</SafeAreaView>
+	);
+};
+
+export default Onboarding;
